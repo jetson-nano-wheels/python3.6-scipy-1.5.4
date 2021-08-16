7160177b5527c0eed18a440514aa94f52c46851c
austinjp <austin.plunkett@gmail.com>
2021-08-16 10:25:34 +0100

    Build with deps

--- build-clean.sh
+++ build-clean.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 ./init.sh
 source venv/bin/activate
 rm -rf dist/*
-pip wheel --no-binary ':all:' --no-deps scipy==1.5.4 -w dist
+pip wheel --no-binary ':all:' scipy==1.5.4 -w dist
--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 ./init.sh
 source venv/bin/activate
-pip wheel --no-binary ':all:' --no-deps scipy -w dist
+pip wheel --no-binary ':all:' scipy==1.5.4 -w dist
